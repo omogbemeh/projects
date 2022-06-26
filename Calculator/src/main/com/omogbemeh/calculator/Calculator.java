1e5bcfcb9ee9c71b9a1056d6d6d266c7e0f22cca
Praise Omogbemeh <onosithena@gmail.com>
2022-06-25 22:27:13 -0400

    Added the boiler plate code for junit

---
+++ Calculator/src/main/com/omogbemeh/calculator/Calculator.java
@@ -0,0 +1,7 @@
+package com.omogbemeh.calculator;
+
+public class Calculator {
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+}
---
+++ Calculator/src/test/com/omogbemeh/calculator/CalculatorTest.java
@@ -0,0 +1,8 @@
+package com.omogbemeh.calculator;
+
+public class CalculatorTest {
+    public void canAddZeroPlusZero() {
+        Calculator calc = new Calculator();
+        int sum = calc.add(0, 0);
+    }
+}
