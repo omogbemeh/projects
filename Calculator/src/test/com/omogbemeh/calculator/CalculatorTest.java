85e67933f6d17496d8eee0fa081e6810feb64bc0
Praise Omogbemeh <onosithena@gmail.com>
2022-06-26 23:38:09 -0400

    Wrote my first java test

--- Calculator/src/test/com/omogbemeh/calculator/CalculatorTest.java
+++ Calculator/src/test/com/omogbemeh/calculator/CalculatorTest.java
@@ -1,8 +1,33 @@
 package com.omogbemeh.calculator;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
+
+    private Calculator calc;
+
+    @BeforeEach
+    void setUp() {
+        calc = new Calculator();
+    }
+
+    @Test
     public void canAddZeroPlusZero() {
-        Calculator calc = new Calculator();
         int sum = calc.add(0, 0);
+        Assertions.assertEquals(0, sum);
+    }
+
+    @Test
+    public void canAddOnePlusOne() {
+        int sum = calc.add(1,1);
+        Assertions.assertEquals(2, sum);
+    }
+
+    @Test
+    public void canAddTwoNegative1() {
+        int sum = calc.add(-1, -1);
+        Assertions.assertEquals(-2, sum);
     }
 }
