fc25a6e756b03f74eac562928419120bbbb41ac4
Praise Omogbemeh <onosithena@gmail.com>
2022-06-27 01:59:36 -0400

    finished v1 of the calculator project

--- Calculator/src/main/com/omogbemeh/calculator/Calculator.java
+++ Calculator/src/main/com/omogbemeh/calculator/Calculator.java
@@ -1,7 +1,31 @@
 package com.omogbemeh.calculator;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import java.text.ParseException;
+
 public class Calculator {
     public static int add(int num1, int num2) {
         return num1 + num2;
+    }
+
+    public String calcAnnuity(String capital, String rate, String interest, String time) throws ParseException {
+        NumberFormat nf = NumberFormat.getCurrencyInstance();
+        String parsedCapital = nf.parse(capital).toString();
+        BigDecimal R = new BigDecimal(parsedCapital);
+        BigDecimal r = new BigDecimal(rate);
+        BigDecimal n = new BigDecimal(interest);
+        BigDecimal t = new BigDecimal(time);
+        BigDecimal rOverN = r.divide(n);
+        BigDecimal nt = n.multiply(t);
+        BigDecimal onePlusROverNToPowNT = BigDecimal.ONE.add(rOverN);
+        BigDecimal onePlusROverNToPowNT2 = onePlusROverNToPowNT.pow(nt.intValue());
+        BigDecimal onePlusROverNToPowNTMinusOne = onePlusROverNToPowNT2.subtract(BigDecimal.ONE);
+        BigDecimal onePlusROverNToPowNTMinusOneDividedByROverN = onePlusROverNToPowNTMinusOne.divide(rOverN);
+        BigDecimal finalAnswer = R.multiply(onePlusROverNToPowNTMinusOneDividedByROverN);
+        String finalCurr = nf.format(finalAnswer);
+        return finalCurr;
     }
 }
--- Calculator/src/test/com/omogbemeh/calculator/CalculatorTest.java
+++ Calculator/src/test/com/omogbemeh/calculator/CalculatorTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.text.ParseException;
+
 public class CalculatorTest {
 
     private Calculator calc;
@@ -29,5 +32,17 @@ public class CalculatorTest {
     public void canAddTwoNegative1() {
         int sum = calc.add(-1, -1);
         Assertions.assertEquals(-2, sum);
+    }
+
+    @Test
+    public void canCalcAnnuity() throws ParseException {
+        String annuity = calc.calcAnnuity("$22,000", ".06", "1", "7");
+        Assertions.assertEquals("$184,664.43", annuity);
+    }
+
+    @Test
+    public void canCalcAnnuity2() throws ParseException {
+        String annuity = calc.calcAnnuity("$1,200", ".08", "4", "10");
+        Assertions.assertEquals("$72,482.38", annuity);
     }
 }
